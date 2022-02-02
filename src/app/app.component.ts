7871a14a7e2bfb3208407f79abdfbc87606aa5d9
2ndline <bob.murrell@gmail.com>
2022-02-02 17:11:46 -0600

    Adding Pouch dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuItem } from './interfaces/menu-item.interface';
 import { MenuService } from './services/menu.service';
+import * as PouchDB from 'pouchdb';
 
 @Component({
   selector: 'base-application',
@@ -8,9 +9,11 @@ import { MenuService } from './services/menu.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  pouchdb: any;
   name = 'BaseApplication';
   menuItems: MenuItem[];
   constructor(public menuService: MenuService) {
     this.menuItems = menuService.getMenuItems();
+    this.pouchdb = new PouchDB('pouchform');
   }
 }
